b8bd875d78d92ab6db4c69a269c860c3f3a4996c
loveencounterflow <mail@somewhere.com>
2023-01-05 17:21:41 +0100

    remove all non-pertinent files; fix `package.json` to achieve CommonJS compatibility

--- compose-regexp.js
+++
@@ -1,5 +0,0 @@
-export {
-    atomic, bound, capture, charSet, either, flags,
-    lookAhead, lookBehind, maybe, namedCapture, noBound,
-    notAhead, notBehind, ref, sequence, suffix
-} from "./src/API.js"
